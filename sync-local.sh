04048fd00df2cb83ff81868914ea55b62d78b5d2
Keith Axline <keith@axline.io>
2024-09-30 11:15:32 -0700

    Configuring for Github Pages

---
+++ sync-local.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Exit script if any command fails
+set -e
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+  export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
+fi
+
+# Remove the target directory if it already exists
+if [ -d "$TARGET_DIR" ]; then
+  rm -rf "$TARGET_DIR"
+fi
+
+cp -r $SOURCE_DIR $TARGET_DIR
+
+chokidar "$SOURCE_DIR**/*" -c "rsync -av --delete $SOURCE_DIR $TARGET_DIR"
+
+echo "Files synced successfully from $SOURCE_DIR to $TARGET_DIR."
